906f69cbaa643104ef32c9f5c73699435dc77f07
Bravoadahson <bravoadahson@gmail.com>
2022-08-12 13:38:59 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import Navbar from "./Pages/NavBar";
+import Hero from "./Pages/Hero";
+import Platform from "./Pages/Platform";
+import Testimonies from "./Pages/Testimonies";
+import FAQ from "./FAQ";
+import Footer from "./Footer";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <Platform />
+      <Testimonies />
+      <FAQ />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
